76326bd7138d93c4a8ab6b3a33280e5fd176c411
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-06-25 16:51:07 -0500

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav'
 import Header from './components/Header';
@@ -7,16 +7,21 @@ import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
 
+const currentClass = document.getElementsByClassName('active');
 
 function App() {
+  const [activeNav, setActiveNav] = useState('#');
   return (
 <>
 <Header></Header>
-<Nav></Nav>
-<About></About>
-<Resume></Resume>
-<Projects></Projects>
-<Contact></Contact>
+<Nav activeNav={activeNav}
+setActiveNav={setActiveNav}></Nav>
+{activeNav === '#' && <About></About>}
+{activeNav === 'resume' && <Resume></Resume>}
+{activeNav === 'projects' && <Projects></Projects>}
+{activeNav === 'contact' && <Contact></Contact>}
+
+
 <Footer></Footer>
 </>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './contact.css';
+import {MdOutlineMailOutline} from 'react-icons/md';
+import {AiFillGithub} from 'react-icons/ai';
+import {AiFillLinkedin} from 'react-icons/ai'
+
 
 function Contact(){
-    
+    return(
     <section id="contact">
-            Contact
+            <h5> Get in touch! </h5>
+            <h2> Contact Me!</h2>
+
+            <div className='container contactContainer'>
+                <div className='contactOptions'>
+                <article className='contactOption'>
+                    <MdOutlineMailOutline className='contactOption-icon' />
+                    <h4>Email</h4>
+                    
+                    <a href='mailto:thomasmccalister@gmail.com'>Send a Message</a>
+                </article>
+                <article className='contactOption'>
+                    <AiFillLinkedin className='contactOption-icon'/>
+                    <h4>LinkedIn</h4>
+                    
+                    <a href='https://www.linkedin.com/in/thomas-mccalister-1b857917a'>Find me on LinkedIn!</a>
+                </article>
+                <article className='contactOption'>
+                    <AiFillGithub className='contactOption-icon'/>
+                    <h4>Github</h4>
+                    
+                    <a href='https://github.com/kyrotomato'>Check out my Github!</a>
+                </article>
+                </div>
+                <form action='='>
+                    <input type='text' name='name' placeholder='Your Full Name' required />
+                    <input type='email' name='email' placeholder='Your Email' required />
+                    <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                </form>
+            </div>
             </section>
-    
+    )
 }
 
 export default Contact
